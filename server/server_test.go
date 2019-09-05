5fa41afa5f62d9e4a34ad0c756101a5e0c8919a0
Franciszek Jełowicki <franek.jelowicki@gmail.com>
2019-09-05 20:55:51 +0200

    Make main server test. Run Sok instance in sokserver/testdata and try to connect

--- server/server_test.go
+++ server/server_test.go
@@ -1,9 +1,24 @@
 package server
 
 import (
+	"net"
 	"testing"
+	"time"
 )
 
 func TestServer(t *testing.T) {
+	go InitServer("./testdata/")
 
+	var conn net.Conn
+	for i := 0; i < 50; i++ { //Try connect to server. If you can't in 5s throw error
+		var err error
+		conn, err = net.Dial("tcp", "127.0.0.1:19151")
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+	if conn == nil {
+		t.Error("Cannot connect to server")
+	}
 }
